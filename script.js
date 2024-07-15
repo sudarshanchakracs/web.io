4991aaae6b7e09bafbc83e7f37f7b1ff9f36e6c9
sudarshan chakra cyber security <sudarsanachakra.cs@gmail.com>
2024-07-15 11:38:51 +0530

    Create script.js\

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? 'block' : 'none';
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function previousSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showSlide(currentSlide);
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
